211cc0c78dae1d104eff36166062b67bdd8ce4be
SunnySharma13 <91840606+SunnySharma13@users.noreply.github.com>
2021-10-03 22:39:18 +0530

    Create odd_set.cpp

---
+++ odd_set.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[2*n];
+        int even=0, odd=0;
+        for(int i=0; i<2*n; ++i){
+            cin>>arr[i];
+        }
+        for(int i=0; i<2*n; ++i){
+            if(arr[i]%2==0){
+                even++;
+            }
+            else
+                odd++;
+        }
+        if(even==odd)
+            cout<<"Yes"<<"\n";
+        else
+            cout<<"No"<<"\n";
+    }
+    return 0;
+}
